0c27fef0681fadcce538750aa3cd01dd6c923af9
Ko Ko Zaw <miscellaneous.thinktank@gmail.com>
2023-04-01 16:14:43 +0630

    1st commit

--- README.md
+++ README.md
@@ -1,2 +1,22 @@
-# GettingAndCleaningDataCourseProject
-"Getting and Cleaning Data" Course's Project 
+## Getting and Cleaning Data Course Project
+
+## Files of the Project
+
+1. Analysis R Script ("run_analysis.R")
+
+This is R script for all steps of analysis that finally produces the tidy data set named "tidy_data.txt".  
+
+2. A tidy data set ("tidy_data.txt")
+
+This is the final data set produced by analysis R script "run_analysis.R"  
+
+3. A code book ("CodeBook.md") 
+
+This is the markdown file that describes the variables, the data in "tidy_data.txt" data file and any transformations or work that you performed to clean up the data in that data file. 
+
+4. a README file (README.md)
+
+This is the current markdown and has explained  all output files of the project and how they are connected.
+
+All these four files are uploaded the Github repository "https://github.com/zawkzaw2/GettingAndCleaningDataCourseProject"
+
---
+++ run_analysis.R
@@ -0,0 +1,57 @@
+# Project for "Getting and Cleaning Data" Course  
+
+## Merge the  training and the test sets---- 
+
+### Load the "data.table" package 
+library(data.table)
+library(reshape2)
+
+### Download the data files
+url <- "https://d396qusza40orc.cloudfront.net/getdata%2Fprojectfiles%2FUCI%20HAR%20Dataset.zip"  
+download.file(url, destfile = "./data/project_data.zip", method = "curl")
+unzip("./data/project_data.zip", exdir = "./data")
+
+path <- "C:/docs/courseraplus/getting_and_cleaning_data/data"
+
+# Load activity labels + features
+activityLabels <- fread(file.path(path, "UCI HAR Dataset/activity_labels.txt")
+                        , col.names = c("classLabels", "activityName"))
+features <- fread(file.path(path, "UCI HAR Dataset/features.txt")
+                  , col.names = c("index", "featureNames"))
+
+featuresWanted <- (grep)("(mean|std)\\(\\)", features[, featureNames])
+measurements <- features[featuresWanted, featureNames]
+measurements <- gsub('[()]', '', measurements)
+
+# Load train datasets
+train <- fread(file.path(path, "UCI HAR Dataset/train/X_train.txt"))[, featuresWanted, with = FALSE]
+data.table::setnames(train, colnames(train), measurements)
+trainActivities <- fread(file.path(path, "UCI HAR Dataset/train/Y_train.txt")
+                       , col.names = c("Activity"))
+trainSubjects <- fread(file.path(path, "UCI HAR Dataset/train/subject_train.txt")
+                       , col.names = c("SubjectNum"))
+train <- cbind(trainSubjects, trainActivities, train)
+
+# Load test datasets
+test <- fread(file.path(path, "UCI HAR Dataset/test/X_test.txt"))[, featuresWanted, with = FALSE]
+data.table::setnames(test, colnames(test), measurements)
+testActivities <- fread(file.path(path, "UCI HAR Dataset/test/Y_test.txt")
+                        , col.names = c("Activity"))
+testSubjects <- fread(file.path(path, "UCI HAR Dataset/test/subject_test.txt")
+                      , col.names = c("SubjectNum"))
+test <- cbind(testSubjects, testActivities, test)
+
+# merge datasets
+combined <- rbind(train, test)
+
+# Convert classLabels to activityName  
+combined[["Activity"]] <- factor(combined[, Activity]
+                              , levels = activityLabels[["classLabels"]]
+                              , labels = activityLabels[["activityName"]])
+
+combined[["SubjectNum"]] <- as.factor(combined[, SubjectNum])
+combined <- melt(data = combined, id = c("SubjectNum", "Activity"))
+combined <- dcast(data = combined, SubjectNum + Activity ~ variable, fun.aggregate = mean)
+
+fwrite(x = combined, file.path(path, "UCI HAR Dataset/tidy_data.txt"), quote = FALSE)
+
